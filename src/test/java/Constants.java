9d524aaaf7015d920496b0956f0cf824f73a4dd2
MichaelKhasin <khasin13@gmail.com>
2020-10-22 20:11:06 +0300

    BuyMe Sanity Front End - initial2

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 class Constants{
     public static final String MY_EMAIL = "khasin13@gmail.com";
-    public static final String MY_PASSWORD = "Tomer1noam";
+    public static final String MY_PASSWORD = "******";
 
 
 }
